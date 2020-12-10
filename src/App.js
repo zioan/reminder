d0c98bff6a832e4f3ef0b33e9ab3e88b9af61ac2
zioan <zaharia.ioan@gmail.com>
2020-12-10 11:28:20 +0100

    more

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="custom-header">My Custom Header</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           On the fly change.
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./button.module.css";
 
 export default function Button(props) {
   let myClick = function () {
@@ -6,6 +7,7 @@ export default function Button(props) {
   };
   return (
     <>
+      <h1 className={styles.customHeader}>Testing</h1>
       <button id={props.id} className={props.className} onClick={myClick}>
         Press Me
       </button>
